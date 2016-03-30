b529a280e2e888b74f45cda0591095eb391214a4
FeCheng <1802397560@qq.com>
2016-03-30 15:04:52 +0800

    Create personalCenter.js

---
+++ widget/script/personalCenter.js
@@ -0,0 +1,21 @@
+var personalCenter = api.require('personalCenter');
+personalCenter.open({
+        imgPath: 'widget://res/personalCenter1.png',
+        placeholderImg: 'widget://res/personalCenter.png',
+        btnArray: [{
+            bgImg: 'widget://res/personal_btn_nor.png',
+            selectedImg: 'widget://res/personal_btn_sele.png'
+        }, {
+           bgImg: 'widget://res/personal_btn_nor.png',
+           selectedImg: 'widget://res/personal_btn_sele.png'
+        }, {
+            bgImg: 'widget://res/personal_btn_nor.png',
+            selectedImg: 'widget://res/personal_btn_sele.png'
+        }]
+},function( ret, err ){
+    if( ret ){
+         alert( JSON.stringify( ret ) );
+    }else{
+         alert( JSON.stringify( err ) );
+    }
+});
